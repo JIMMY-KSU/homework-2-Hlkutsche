6bab3d5c95e15e4c0f20c88f50b95211f60a7e06
mgabdo <mgabdo@ksu.edu>
2018-09-11 12:15:49 -0500

    Adding converting to Kelvin capability

--- temperature.sh
+++ temperature.sh
@@ -1,4 +1,9 @@
+#!bin/bash
 echo -n -e "Input temperature in Fahrenheit: "
 read F
-echo "scale=1; ($F-32)*(5/9)" |bc
-
+echo "scale=1; ($F-32)*(5/9)" |bc #This is causing false calculations, you may consider removing the round brackets around 5/9
+#You can test your converter by adding -40 deg F which should be equivalent to -40 deg C  
+C=$(echo "scale=2;($F-32)*5/9" | bc)
+K0=273.15
+K=$(echo "scale=2;$C+$K0" | bc)
+echo "$F degree Fahrenheit is equivalent to $C degree Celsius and $K degree Kelvin"
